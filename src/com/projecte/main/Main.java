5f624a1a7626f00333f581df1613a1b499035376
Sergi Blasco Gimenez <sblascogimenez@gmail.com>
2023-02-24 11:54:12 +0100

    Mostrar Sergi afegit

---
+++ src/com/projecte/main/Main.java
@@ -0,0 +1,25 @@
+package com.projecte.main;
+
+//import com.projecte.Alex.MostrarAlex;
+import com.projecte.sergi.MostrarSergi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		MostrarSergi mostrarsergi = new MostrarSergi();
+//		MostrarAlex mostraralex = new MostrarAlex();
+		mostrarsergi.mostrarSergi();
+//		mostraralex.mostrarAlex();
+
+	}
+
+}
+
+//mostrar nom alex
+//package com.projecte.Alex;
+//
+//public class MostrarAlex {
+//    public void mostrarAlex() {
+//        System.out.println("Alex Garcia");
+//    }
+//}
---
+++ src/com/projecte/sergi/MostrarSergi.java
@@ -0,0 +1,7 @@
+package com.projecte.sergi;
+
+public class MostrarSergi {
+	public void mostrarSergi() {
+		System.out.println("Sergi Blasco Gimenez");
+	}
+}
